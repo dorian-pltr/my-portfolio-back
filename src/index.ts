63ac389fb2c928bcc2769c981f4d402d5852e01d
urbadorian <pelletier.dorian@urbasolar.com>
2024-01-14 12:25:11 +0100

    feat: add db ping cron to avoid application shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 // src/index.ts
 
 import { createSchema, createYoga } from "graphql-yoga";
+import cron from "node-cron";
 import { createServer } from "node:http";
+import { prisma } from "./db";
 import { schema } from "./schema";
 
 const yoga = createYoga({
@@ -20,4 +22,18 @@ const port = Number(process.env.PORT) || 4000;
 
 server.listen(port, () => {
   console.info(`Server is running on ${url}:${port}/graphql`);
+});
+
+cron.schedule("0 * * * *", async () => {
+  try {
+    const result = await prisma.$executeRaw`SELECT 1`;
+
+    if (result) {
+      console.info("Base de données accessible !");
+    } else {
+      console.error("Échec de la requête ping à la base de données.");
+    }
+  } catch (error) {
+    console.error("Erreur lors du ping de la base de données :", error);
+  }
 });
